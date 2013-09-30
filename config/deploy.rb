78fee202a1c515bf094d9bb2ec6938486d0aa000
Scott V. Rosenthal <sr7575@gmail.com>
2013-09-30 14:16:19 -0500

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :chef_environments, %w(staging production)
 # chmod 600 /your-full/local-path-to-ssh-key/xxxxx_keypair_ec2.pem
 # ssh-add /your-full/local-path-to-ssh-key/xxxxx_keypair_ec2.pem
 
-ssh_options[:keys] = File.expand_path('~/ssh_keys/wrtp_keypair_east_ec2.pem')
+ssh_options[:keys] = File.expand_path('~/ssh_keys/xxxxx_keypair_east_ec2.pem')
 default_run_options[:pty] = true
 
 set :user, 'ec2-user'
