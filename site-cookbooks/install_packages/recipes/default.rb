d7928e79eeaa4dd5a985737e0ada2504bb7de6a7
Scott V. Rosenthal <sr7575@gmail.com>
2014-07-17 14:40:46 -0500

    Update default.rb
    add ruby deps

--- site-cookbooks/install_packages/recipes/default.rb
+++ site-cookbooks/install_packages/recipes/default.rb
@@ -15,26 +15,51 @@ landscape-client
 end
 
 # packages to install
-%w(
-gcc gcc-c++ 
-compat-libstdc++-33
-libstdc++-devel
-elfutils-libelf-devel
-sysstat
-glibc-devel
-libaio-devel
-libxml2 libxml2-devel 
-libxslt libxslt-devel
-vim-enhanced screen procps
-sqlite-devel ntp wget curl
-zlib zlib-devel curl-devel
-ImageMagick ImageMagick-devel
-libyaml-devel libffi-devel
-autoconf automake libtool bison
-make openssl openssl-devel
-git expect pcre pcre-devel
-readline-devel
-).each do |pkg|
+["ImageMagick",
+ "ImageMagick-devel",
+ "autoconf",
+ "automake",
+ "bison",
+ "bzip2",
+ "compat-libstdc++-33",
+ "curl",
+ "curl-devel",
+ "elfutils-libelf-devel",
+ "expect",
+ "gcc",
+ "gcc-c++",
+ "git",
+ "glibc-devel",
+ "libaio-devel",
+ "libffi-devel",
+ "libstdc++-devel",
+ "libtool",
+ "libxml2",
+ "libxml2-devel",
+ "libxslt",
+ "libxslt-devel",
+ "libyaml-devel",
+ "make",
+ "mysql",
+ "mysql-devel",
+ "nginx",
+ "nodejs",
+ "ntp",
+ "openssl",
+ "openssl-devel",
+ "patch",
+ "pcre",
+ "pcre-devel",
+ "procps",
+ "readline",
+ "readline-devel",
+ "screen",
+ "sqlite-devel",
+ "sysstat",
+ "vim-enhanced",
+ "wget",
+ "zlib",
+ "zlib-devel"].each do |pkg|
   package pkg do
     retries 2
     retry_delay 10
@@ -44,3 +69,5 @@ end
 
 
 execute 'yum clean dbcache'
+execute 'yum clean all'
+execute 'yum makecache'
